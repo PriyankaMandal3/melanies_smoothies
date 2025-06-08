53cd431a96898411fd8411fd5bac216f388e4411
PriyankaMandal3 <priyankamandal3101@gmail.com>
2025-06-08 13:31:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,30 +19,31 @@ name_on_order = st.text_input('Smoothie Name:')
 st.write('Name of your Smoothie will be:', name_on_order)
 
 # Get fruit options from the table
-my_dataframe = session.table("smoothies.public.fruit_options") \
-                    .select(col('FRUIT_NAME'), col('SEARCH_ON')) \
-                    .to_pandas()
+my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+                  
 
 # Multiselect input
 ingredients_list = st.multiselect(
-    'Choose upto five ingredients:', 
-    my_dataframe['FRUIT_NAME'].tolist(),
-    max_selections=5
+    'Choose upto five ingredients:'
+    , my_dataframe
+    , max_selections=5
 )
 
 # Handle selection
 if ingredients_list:
-    ingredients_string = ', '.join(ingredients_list)
+    ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
-        # ingredients_string += fruit_chosen + ' '
+        ingredients_string += fruit_chosen + ' '
         # Just adding the two lines exactly as shown
-        search_on_value = my_dataframe.loc[
-            my_dataframe['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'
-        ].values[0]
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on_value)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = f"""insert into smoothies.public.orders(ingredients, name_on_order)
