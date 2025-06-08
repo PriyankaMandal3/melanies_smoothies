cf232f7007f3d460363696b4fbce9ddd969d3037
PriyankaMandal3 <priyankamandal3101@gmail.com>
2025-06-08 15:57:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,23 +34,20 @@ ingredients_list = st.multiselect(
 
 # Handle selection
 if ingredients_list:
-    ingredients_string = ''
+        ingredients_string = ''
     
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        # Just adding the two lines exactly as shown
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        for fruit_chosen in ingredients_list:
+                ingredients_string += fruit_chosen + ' '
+                search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+                st.subheader(fruit_chosen + ' Nutrition Information')
+                smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+                sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
-    my_insert_stmt = f"""insert into smoothies.public.orders(ingredients, name_on_order)
+        my_insert_stmt = f"""insert into smoothies.public.orders(ingredients, name_on_order)
                          values ('{ingredients_string}', '{name_on_order}')"""
 
-    time_to_insert = st.button('Submit Order')
-    if time_to_insert:
+        time_to_insert = st.button('Submit Order')
+        if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
